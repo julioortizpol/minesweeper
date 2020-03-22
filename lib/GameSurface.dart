2f077bef5c58749ba675116b4e13c5be49e4d15e
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-03-21 20:13:22 -0400

    change AppBar widget for MinesweeperAppBar

--- lib/GameSurface.dart
+++ lib/GameSurface.dart
@@ -1,31 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mineskeeper/GridBoard.dart';
+import 'package:mineskeeper/MineSweeperAppBar.dart';
 
 class GameSurface extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.teal[200],
-          title: Text("MinesSweper"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.settings,
-                color: Colors.white,
-              ),
-              tooltip: "Settings",
-              onPressed: () {},
-            ),
-          ],
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(40),
-            child: Row(
-              children: <Widget>[],
-            ),
-          ),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100),
+          child: MineSweeperAppBar(),
         ),
         body: SafeArea(
           child: Container(
